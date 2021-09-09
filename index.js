598c788b0934e77568760c66e8d83ae48b948c61
Thomas <thomasmckenna12gmail.com>
2021-09-09 10:41:30 -0400

    commit tree 6fff46f20bc0163b56dbee663296549282c8c680

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+     return string.charAt().toUpperCase() + string.slice(1);
+  }
+  static sanitize(string) {
+    return string.replace(/[^0-9\-]/g,'')
+   }
+  static titleize(string) {
+}
 }
